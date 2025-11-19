43747d7bfbb97076edf8c8b4f13d7ec3121a73d7
Tyler Le <tyler0308gm@gmail.com>
2025-11-18 19:52:42 -0800

    Pushed project to git

---
+++ src/main/java/com/tle/security/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.tle.security.repository;
+
+import com.tle.security.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long>
+{
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByVerificationCode(String verificationCode);
+
+}
+
+
